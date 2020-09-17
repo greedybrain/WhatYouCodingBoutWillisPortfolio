711a7ea72fd4742a275c1e6631d211e8e04485a7
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 11:02:22 -0400

    created EditorParentContainer

---
+++ src/Pages/EditorPage/containers/EditorParentContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditorParentContainer = () => {
+        return (
+                <div>
+                        
+                </div>
+        )
+}
+
+export default EditorParentContainer
