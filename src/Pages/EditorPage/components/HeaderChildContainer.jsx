a72327429d9fa1eee804e5233052eb0fd779ddd0
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 11:02:03 -0400

    created HeaderChildContainer

---
+++ src/Pages/EditorPage/components/HeaderChildContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderChildContainer = () => {
+        return (
+                <div>
+                        
+                </div>
+        )
+}
+
+export default HeaderChildContainer
