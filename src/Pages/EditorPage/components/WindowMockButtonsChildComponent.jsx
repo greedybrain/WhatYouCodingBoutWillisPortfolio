341be93dbe4da3db3bec27784bc1a4fe5b1d996c
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 11:51:14 -0400

    added mock buttons

---
+++ src/Pages/EditorPage/components/WindowMockButtonsChildComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../../../Assets/WindowMockButtonsChildComponentStyles/windowMockButtonsChildComponent.css'
+
+const WindowMockButtonsChildComponent = () => {
+        return (
+                <div className="window_mock_buttons_child_component">
+                        <ul>
+                                <li className="red"></li>
+                                <li className="yellow"></li>
+                                <li className="green"></li>
+                        </ul>
+                </div>
+        )
+}
+
+export default WindowMockButtonsChildComponent
