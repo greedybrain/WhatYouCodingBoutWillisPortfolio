14a33471a06828d14f190643dcbe4de3a77e8e98
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 14:28:20 -0400

    created LeftSpaceChildContainer

---
+++ src/Pages/EditorPage/components/LeftSpaceChildContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../../../Assets/LeftSideWSStyles/leftSide.css'
+
+const LeftSpaceChildContainer = () => {
+        return (
+                <div className="left_side">
+                        
+                </div>
+        )
+}
+
+export default LeftSpaceChildContainer
