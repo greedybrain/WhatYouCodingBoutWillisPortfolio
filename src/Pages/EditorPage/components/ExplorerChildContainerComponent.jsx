51a52d00f48c680fd4b71f0024c28800a4c44655
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 13:28:33 -0400

    intergrated navbar component

---
+++ src/Pages/EditorPage/components/ExplorerChildContainerComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavbarChildComponent from './NavbarChildComponent'
+import '../../../Assets/ExplorerChildContainerStyles/explorerChildContainer.css'
+
+const ExplorerChildContainerComponent = () => {
+        return (
+                <div className="explorer_container">
+                        <NavbarChildComponent />
+                </div>
+        )
+}
+
+export default ExplorerChildContainerComponent
