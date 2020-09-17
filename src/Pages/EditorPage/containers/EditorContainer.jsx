1b2b42766dbaeed1de79be06510f8485525c4c62
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 10:58:30 -0400

    created editor contianer component

---
+++ src/Pages/EditorPage/containers/EditorContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const EditorContainer = () => {
+        return (
+                <div>
+                        {/* HEADER 
+                        EDITOR BODY 
+                        FOOTER  */}
+                </div>
+        )
+}
+
+export default EditorContainer
