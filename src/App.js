d5df49a03066ccb1c29c3226b2691921153ba1cf
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 11:49:18 -0400

    imported editor container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import EditorParentContainer from './Pages/EditorPage/containers/EditorParentContainer';
+import './Assets/EditorParentContainerStyles/editorParentContainer.css'
+
 function App() {
   return (
     <div className="App">
+      <EditorParentContainer />
     </div>
   );
 }
