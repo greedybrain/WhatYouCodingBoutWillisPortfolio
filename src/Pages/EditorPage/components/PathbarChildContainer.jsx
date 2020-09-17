382b51ccdd1ce0b38cb7e44882033a5f86bf7da2
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 13:29:04 -0400

    created pathbar component

---
+++ src/Pages/EditorPage/components/PathbarChildContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import '../../../Assets/PathBarChildContainerStyles/pathBarChildContainer.css'
+
+const PathbarChildContainer = () => {
+        return (
+                <div className="pathbar">
+                        <ul>
+                                <li>READ.me</li>
+                                <li>PROJECTS.mine</li>
+                        </ul>
+                        <div className="path">
+                                Naya Willis &gt; READ.me
+                        </div>
+                </div>
+        )
+}
+
+export default PathbarChildContainer
