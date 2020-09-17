eab1990ee6bb6c637f7f5db48c53fd608f4dcc1b
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 13:29:26 -0400

    integrated editor body component

--- src/Pages/EditorPage/containers/EditorParentContainer.jsx
+++ src/Pages/EditorPage/containers/EditorParentContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import EditorBodyChildContainer from '../components/EditorBodyChildContainer'
 import HeaderChildContainer from '../components/HeaderChildContainer'
 
 const EditorParentContainer = () => {
         return (
                 <div className="editor_container">
                         <HeaderChildContainer />
+                        <EditorBodyChildContainer />
                 </div>
         )
 }
