eef3801f12bc253a2bee8ac07a1c26a00f486d51
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 13:28:15 -0400

    integrated explorer and inner ws components

--- src/Pages/EditorPage/components/EditorBodyChildContainer.jsx
+++ src/Pages/EditorPage/components/EditorBodyChildContainer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import '../../../Assets/EditorBodyChildContainerStyles/editorBodyChildContainer.css'
+import ExplorerChildContainerComponent from './ExplorerChildContainerComponent'
+import InnerWorkspaceChildContainer from './InnerWorkspaceChildContainer'
+
 const EditorBodyChildContainer = () => {
         return (
-                <div>
-                        
+                <div className="editor_body_container">
+                        <ExplorerChildContainerComponent />
+                        <InnerWorkspaceChildContainer />
                 </div>
         )
 }
