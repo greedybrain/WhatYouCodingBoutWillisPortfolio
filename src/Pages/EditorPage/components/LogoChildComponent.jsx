28922b31e3eed40dab55157072663754396db0b7
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 11:51:04 -0400

    added logo

---
+++ src/Pages/EditorPage/components/LogoChildComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../../../Assets/LogoChildComponentStyles/logoChildComponent.css'
+
+const LogoChildComponent = () => {
+        return (
+                <div className="logo">
+                        WhatYou&lt;/Coding&gt;BoutWillis!
+                </div>
+        )
+}
+
+export default LogoChildComponent
