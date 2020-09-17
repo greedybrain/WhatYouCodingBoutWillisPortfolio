ce7ac5d62f7bb3ff1c7534b3ef4086e2b464fcd6
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 14:28:32 -0400

    created MinimapChildComponent

---
+++ src/Pages/EditorPage/components/MinimapChildComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../../../Assets/MiniMapChildComponentStyles/minimap.css'
+
+const MinimapChildComponent = () => {
+        return (
+                <div className="minimap">
+                        <img src="" alt=""/>
+                </div>
+        )
+}
+
+export default MinimapChildComponent
