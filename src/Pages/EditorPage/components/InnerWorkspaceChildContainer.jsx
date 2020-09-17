8953aea70e4c051e1abb2154d31ffc39a34ade42
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 14:27:58 -0400

    integrated splitescreen component

--- src/Pages/EditorPage/components/InnerWorkspaceChildContainer.jsx
+++ src/Pages/EditorPage/components/InnerWorkspaceChildContainer.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import PathbarChildContainer from './PathbarChildContainer'
 
 import '../../../Assets/InnerWorkspaceChildContainerStyles/innerWorkspaceChildContainer.css'
+import SplitScreenChildContainer from './SplitScreenChildContainer'
 
 const InnerWorkspaceChildContainer = () => {
         return (
                 <div className="inner_workspace_container">
                         <PathbarChildContainer />
+                        <SplitScreenChildContainer />
                 </div>
         )
 }
