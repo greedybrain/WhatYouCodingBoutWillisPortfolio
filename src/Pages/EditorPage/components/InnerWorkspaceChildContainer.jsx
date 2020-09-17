7db09389f42bf167ce31b028864c0b8426d93b5d
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 13:28:43 -0400

    integrated pathbar component

---
+++ src/Pages/EditorPage/components/InnerWorkspaceChildContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PathbarChildContainer from './PathbarChildContainer'
+
+import '../../../Assets/InnerWorkspaceChildContainerStyles/innerWorkspaceChildContainer.css'
+
+const InnerWorkspaceChildContainer = () => {
+        return (
+                <div className="inner_workspace_container">
+                        <PathbarChildContainer />
+                </div>
+        )
+}
+
+export default InnerWorkspaceChildContainer
