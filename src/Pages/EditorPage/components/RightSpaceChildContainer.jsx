6a19334bc1e50de3b6617af546593cc8906faf2c
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-17 14:29:03 -0400

    created RightSpaceChildContainer

---
+++ src/Pages/EditorPage/components/RightSpaceChildContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../../../Assets/RightSideWSStyles/rightSide.css'
+
+const RightSpaceChildContainer = () => {
+        return (
+                <div className="right_side">
+                        
+                </div>
+        )
+}
+
+export default RightSpaceChildContainer
